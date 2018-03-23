f385874633faaaff5adee965b7e4b6230f8ba1b3
nidhi5885 <nidhisharma5885@gmail.com>
2018-03-23 12:14:04 +0530

    conn.php changed

--- conn.php
+++ conn.php
@@ -6,13 +6,21 @@ $username = "nidhisharma@my-sql-server01";
 $password = "password@123";
 //$password = "";
 $database="lportal";
-$base_url='portal.progressive.in/web/procare/arf';
+$base_url='portal.azure.com';
 
-$conn = mysql_connect($servername, $username, $password);
-$dbcon=mysql_select_db($database);
-if (function_exists('date_default_timezone_set'))
-{
-  date_default_timezone_set('Asia/Kolkata');
+
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ; 
+mysqli_real_connect($conn, $servername, $username, $password, 'quickstartdb', 3306, MYSQLI_CLIENT_SSL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
+
+// $conn = mysql_connect($servername, $username, $password);
+// $dbcon=mysql_select_db($database);
+// if (function_exists('date_default_timezone_set'))
+// {
+//   date_default_timezone_set('Asia/Kolkata');
+// }
 /*Configuration File*/
 ?>
